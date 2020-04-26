dbffacc7cab24501bc229d9333034ab8c1758485
Antonino Modica <antonino.modica@noema.it>
2020-04-26 18:16:56 +0200

    refactor

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {Field, reduxForm} from 'redux-form';
 
-const StreamCreate =()=>{
+class StreamCreate extends Component{
 
-    return <div>StreamCreate</div>;
+
+    renderInput(){
+        return <div>I am input</div>;
+    }
+    render(){
+
+        //console.log(this.props);
+        return (
+        <form>
+            <Field name="title" component={this.renderInput}/>
+            <Field name="description" component={this.renderInput}/>
+        </form>
+         ) ;
+    }
 }
 
 
-export default StreamCreate;
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);
